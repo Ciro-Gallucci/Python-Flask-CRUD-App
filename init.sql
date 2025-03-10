23634138126a2373f307e4eb9dd9e3d5b67b7152
Ciro <52879334+Ciro-Gallucci@users.noreply.github.com>
2025-03-10 11:39:58 +0100

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,6 +1,11 @@
+-- Crea la tabella students
 CREATE TABLE IF NOT EXISTS students (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  email VARCHAR(100) NOT NULL,
-  phone VARCHAR(15) NOT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone VARCHAR(15) NOT NULL
 );
+
+-- Concedi tutti i privilegi all'utente 'user'
+GRANT ALL PRIVILEGES ON students_db.* TO 'user'@'%';
+FLUSH PRIVILEGES;
